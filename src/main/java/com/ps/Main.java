7d2624152cce60d89292d1d0270f84ea03d1d778
Sallie Sheppard <ssheppard@myemailprovider.com>
2024-04-12 13:36:00 -0400

    Scanner implementation in main method

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,9 +1,17 @@
 package com.ps;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Which calculator would you like to choose?\n1. Mortgage Calculator\n2. Future Value Calculator\n3. Present Value Calculator");
+
+        byte studentAnswer = scanner.nextByte();
+
+
 
     }
 }
